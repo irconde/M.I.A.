2c601c12782787939314b8a3a11a145874e7a2ba
Stephanie <srpatton@ualr.edu>
2020-07-07 10:32:47 -0500

    Added Zoom and Pan tools from cornerstone. Created information and detection overlay on uploaded image.

--- src/App.js
+++ src/App.js
@@ -6,22 +6,41 @@ import * as cornerstoneTools from "cornerstone-tools";
 import dicomParser from 'dicom-parser';
 import * as cornerstoneMath from "cornerstone-math";
 import Hammer from "hammerjs";
-import * as cornerstoneWADOimageLoader from "cornerstone-wado-image-loader";
+import * as cornerstoneWADOImageLoader from "cornerstone-wado-image-loader";
 
 cornerstoneTools.external.cornerstone = cornerstone;
 cornerstoneTools.external.Hammer = Hammer;
 cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
-cornerstoneWADOimageLoader.external.cornerstone = cornerstone;
-cornerstoneWADOimageLoader.external.dicomParser = dicomParser;
+cornerstoneTools.init();
+
+cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
+cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
+cornerstoneWADOImageLoader.webWorkerManager.initialize({
+  maxWebWorkers: navigator.hardwareConcurrency || 1,
+  startWebWorkersOnDemand: true,
+  taskConfiguration: {
+    decodeTask: {
+      initializeCodecsOnStartup: false,
+      usePDFJS: false,
+      strict: false,
+    },
+  },
+});
 
 let loaded = false;
 
 class App extends Component {
-
-  state = {
-    // Initially, no file is selected
-    selectedFile: null
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      // Initially, no file is selected
+      selectedFile: null,
+      viewport: cornerstone.getDefaultViewport(null, undefined),
+    };
+    this.onFileChange = this.onFileChange.bind(this);
+    this.onFileUpload = this.onFileUpload.bind(this);
+    this.loadAndViewImage = this.loadAndViewImage.bind(this);
+  }
 
   // On file select (from the pop up)
   onFileChange = event => {
@@ -35,20 +54,15 @@ class App extends Component {
   // On file upload (click the upload button)
   onFileUpload = (file) => {
 
-    const imageId = cornerstoneWADOimageLoader.wadouri.fileManager.add(file);
-    console.log(imageId);
+    const imageId = cornerstoneWADOImageLoader.wadouri.fileManager.add(file);
     this.loadAndViewImage(imageId);
   };
 
   loadAndViewImage(imageId) {
-    cornerstoneTools.init();
-
     const element = document.getElementById('dicomImage');
     cornerstone.enable(element);
 
-    const start = new Date().getTime();
     cornerstone.loadImage(imageId).then(function(image) {
-      console.log(image);
       const viewport = cornerstone.getDefaultViewportForImage(element, image);
       // document.getElementById('toggleModalityLUT').checked = (viewport.modalityLUT !== undefined);
       // document.getElementById('toggleVOILUT').checked = (viewport.voiLUT !== undefined);
@@ -65,6 +79,29 @@ class App extends Component {
         loaded = true;
       }
 
+      function setTools(){
+        // IMAGE PAN
+        const PanTool = cornerstoneTools.PanTool;
+        cornerstoneTools.addTool(PanTool)
+        cornerstoneTools.setToolActive('Pan', { mouseButtonMask: 1 })
+
+        // ZOOM
+        // Add our tool, and set it's mode
+        const Zoom = cornerstoneTools.ZoomTool;
+        cornerstoneTools.addTool(Zoom)
+        cornerstoneTools.setToolActive("ZoomMouseWheel", {});
+
+        const ZoomTouchPinchTool = cornerstoneTools.ZoomTouchPinchTool;
+        cornerstoneTools.addTool(ZoomTouchPinchTool)
+        cornerstoneTools.setToolActive('ZoomTouchPinch', {})
+
+        // RECTANGLE ROI
+        const RectangleRoiTool = cornerstoneTools.RectangleRoiTool;
+        cornerstoneTools.addTool(RectangleRoiTool)
+        cornerstoneTools.setToolActive('RectangleRoi', { mouseButtonMask: 2 })
+      }
+      setTools();
+
       // function getTransferSyntax() {
       //   const value = image.data.string('x00020010');
       //   return value + ' [' + uids[value] + ']';
@@ -75,49 +112,92 @@ class App extends Component {
       //   return value + ' [' + uids[value] + ']';
       // }
 
-      function getPixelRepresentation() {
-        const value = image.data.uint16('x00280103');
+      function getStudyDescription() {
+        const value = image.data.string('x00081030');
         if(value === undefined) {
           return;
         }
         return value + (value === 0 ? ' (unsigned)' : ' (signed)');
       }
 
-      function getPlanarConfiguration() {
-        const value = image.data.uint16('x00280006');
+      function getInstitutionName() {
+        const value = image.data.string('x00080080');
         if(value === undefined) {
           return;
         }
-        return value + (value === 0 ? ' (pixel)' : ' (plane)');
+        return value + (value === 0 ? ' (unsigned)' : ' (signed)');
       }
 
-      // document.getElementById('transferSyntax').textContent = getTransferSyntax();
-      // document.getElementById('sopClass').textContent = getSopClass();
-      // document.getElementById('samplesPerPixel').textContent = image.data.uint16('x00280002');
-      // document.getElementById('photometricInterpretation').textContent = image.data.string('x00280004');
-      // document.getElementById('numberOfFrames').textContent = image.data.string('x00280008');
-      // document.getElementById('planarConfiguration').textContent = getPlanarConfiguration();
-      // document.getElementById('rows').textContent = image.data.uint16('x00280010');
-      // document.getElementById('columns').textContent = image.data.uint16('x00280011');
-      // document.getElementById('pixelSpacing').textContent = image.data.string('x00280030');
-      // document.getElementById('bitsAllocated').textContent = image.data.uint16('x00280100');
-      // document.getElementById('bitsStored').textContent = image.data.uint16('x00280101');
-      // document.getElementById('highBit').textContent = image.data.uint16('x00280102');
-      // document.getElementById('pixelRepresentation').textContent = getPixelRepresentation();
-      // document.getElementById('windowCenter').textContent = image.data.string('x00281050');
-      // document.getElementById('windowWidth').textContent = image.data.string('x00281051');
-      // document.getElementById('rescaleIntercept').textContent = image.data.string('x00281052');
-      // document.getElementById('rescaleSlope').textContent = image.data.string('x00281053');
-      // document.getElementById('basicOffsetTable').textContent = image.data.elements.x7fe00010 && image.data.elements.x7fe00010.basicOffsetTable ? image.data.elements.x7fe00010.basicOffsetTable.length : '';
-      // document.getElementById('fragments').textContent = image.data.elements.x7fe00010 && image.data.elements.x7fe00010.fragments ? image.data.elements.x7fe00010.fragments.length : '';
-      // document.getElementById('minStoredPixelValue').textContent = image.minPixelValue;
-      // document.getElementById('maxStoredPixelValue').textContent = image.maxPixelValue;
-      // const end = new Date().getTime();
-      // const time = end - start;
-      // document.getElementById('totalTime').textContent = time + "ms";
-      // document.getElementById('loadTime').textContent = image.loadTimeInMS + "ms";
-      // document.getElementById('decodeTime').textContent = image.decodeTimeInMS + "ms";
-    }, function(err) {
+      function getInstitutionLocation() {
+        const value = image.data.string('x00080081');
+        if(value === undefined) {
+          return;
+        }
+        return value + (value === 0 ? ' (unsigned)' : ' (signed)');
+      }
+
+
+      const detectAlgo = image.data.string('x40101029');
+      const detectType = image.data.string('x00187004');
+      const detectConfig = image.data.string('x00187005');
+
+      const station = image.data.string('x00081010');
+      const series = image.data.string('x0008103e');
+      const study = image.data.string('x00081030');
+      var today = new Date();
+      var dd = String(today.getDate()).padStart(2, '0');
+      var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+      var yyyy = today.getFullYear();
+
+      var seriesTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+      var seriesDate = mm + '/' + dd + '/' + yyyy;
+
+      console.log(image.data.elements.x40101011.items[0].dataSet.elements.x40101037.items[0].dataSet.elements.x4010101d);
+      var pixelData = new Uint8Array(image.data.byteArray.buffer,
+          image.data.elements.x40101011.items[0].dataSet.elements.x40101037.items[0].dataSet.elements.x4010101d.dataOffset,
+          image.data.elements.x40101011.items[0].dataSet.elements.x40101037.items[0].dataSet.elements.x4010101d.length);
+
+      console.log(pixelData);
+      console.log(detectAlgo);
+      console.log(getStudyDescription());
+      console.log(getInstitutionName());
+      console.log(getInstitutionLocation());
+
+
+      // setup handlers before we display the image
+      function onImageRendered(e) {
+        const eventData = e.detail;
+
+        // set the canvas context to the image coordinate system
+        cornerstone.setToPixelCoordinateSystem(eventData.enabledElement, eventData.canvasContext);
+
+        // NOTE: The coordinate system of the canvas is in image pixel space.  Drawing
+        // to location 0,0 will be the top left of the image and rows,columns is the bottom
+        // right.
+        const context = eventData.canvasContext;
+        context.beginPath();
+        context.strokeStyle = '#4ceb34';
+        context.lineWidth = .5;
+        context.rect(190, 410, 110, 110);
+        context.stroke();
+        context.fillStyle = "#4ceb34";
+        context.font = "10px Arial";
+        context.fillText(detectAlgo, 190, 405);
+
+        document.getElementById('topleft').textContent = "Algorithm: " + detectAlgo;
+        document.getElementById('topleft2').textContent = "Detector Type: " + detectType;
+        document.getElementById('topleft3').textContent = "Detector Configuration: " + detectConfig;
+        document.getElementById('topright').textContent = "Station Name: " + station;
+        document.getElementById('topright2').textContent = "Date: " + seriesDate;
+        document.getElementById('topright3').textContent = "Time: " + seriesTime;
+        document.getElementById('bottomleft').textContent = "Series: " + series;
+        document.getElementById('bottomright').textContent = "Zoom:" + eventData.viewport.scale.toFixed(2);
+        document.getElementById('bottomleft2').textContent = "Study: " + study;
+
+      }
+      element.addEventListener('cornerstoneimagerendered', onImageRendered);
+
+      }, function(err) {
       alert(err);
     });
   }
@@ -128,18 +208,17 @@ class App extends Component {
   fileData = () => {
 
     if (this.state.selectedFile) {
-      const imageId = cornerstoneWADOimageLoader.wadouri.fileManager.add(this.state.selectedFile);
+      const imageId = cornerstoneWADOImageLoader.wadouri.fileManager.add(this.state.selectedFile);
       this.loadAndViewImage(imageId);
-      console.log(imageId);
       return (
           <div>
             <h2>File Details:</h2>
             <p>File Name: {this.state.selectedFile.name}</p>
             <p>File Type: {this.state.selectedFile.type}</p>
-            <p>
-              Last Modified:{" "}
-              {this.state.selectedFile.lastModifiedDate.toDateString()}
-            </p>
+            {/*<p>*/}
+            {/*  Last Modified:{" "}*/}
+            {/*  {this.state.selectedFile.lastModifiedDate.toDateString()}*/}
+            {/*</p>*/}
           </div>
       );
     } else {
