bf33bbd2432a500c8d54562e8e9318f4a8186d97
irconde <irconde@ualr.edu>
2020-08-28 15:58:18 -0500

    Minor aesthetical changes

--- src/components/MetaData.js
+++ src/components/MetaData.js
@@ -6,10 +6,10 @@ class MetaData extends Component {
         super(props);
         this.state = {
             spanHeadStyle: {
-                fontWeight: 'normal'
+                fontWeight: '500'
             },
             spanBodyStyle: {
-                fontWeight: 'lighter'
+                fontWeight: '300'
             },
             paragraphStyle: {
                 margin: '0.4rem 0.7rem 0.4rem 0.7rem'
@@ -18,21 +18,20 @@ class MetaData extends Component {
                 backgroundColor: '#979797',
                 height: '0.01rem',
                 border: '0',
-                marginTop: '0.8rem',
+                marginTop: '0.9rem',
                 marginBottom: '0.8rem',
                 width: '95%'
             },
             divStyle: {
                 position: 'absolute',
-                top: '4rem',
+                top: '2rem',
                 left: '2rem',
-                opacity: '75%',
-                backgroundColor: '#282828',
-                borderRadius: '0.3rem',
+                backgroundColor: 'rgba(38, 38, 38, 0.75)',
+                borderRadius: '0.5rem',
                 justifyContent: 'center',
                 textAlign: 'left',
                 color: '#ffffff',
-                padding: '0.2rem 0.5rem',
+                padding: '0.8rem 0.8rem',
                 width: 'auto'
             }
         }
@@ -51,24 +50,24 @@ class MetaData extends Component {
         return (
             <div style={this.state.divStyle}>
                <p style={this.state.paragraphStyle}>
-                   <span style={this.state.spanHeadStyle}>Algorithm:</span> 
+                   <span style={this.state.spanHeadStyle}>Algorithm:</span>
                     <span style={this.state.spanBodyStyle}> {this.props.algorithmType}</span>
-                </p> 
+                </p>
                <p style={this.state.paragraphStyle}>
-                   <span style={this.state.spanHeadStyle}>Detector Type:</span> 
+                   <span style={this.state.spanHeadStyle}>Detector Type:</span>
                     <span style={this.state.spanBodyStyle}> {this.props.detectorType}</span>
-                </p> 
+                </p>
                <p style={this.state.paragraphStyle}>
-                   <span style={this.state.spanHeadStyle}>Detector Configuration:</span> 
+                   <span style={this.state.spanHeadStyle}>Detector Configuration:</span>
                     <span style={this.state.spanBodyStyle}> {this.props.detectorConfigType}</span>
-                </p> 
-                <hr style={this.state.hrStyle} /> 
+                </p>
+                <hr style={this.state.hrStyle} />
                <p style={this.state.paragraphStyle}>
-                   <span style={this.state.spanHeadStyle}>Series:</span> 
+                   <span style={this.state.spanHeadStyle}>Series:</span>
                    <span style={this.state.spanBodyStyle}> {this.props.seriesType}</span>
-                </p> 
+                </p>
                <p style={this.state.paragraphStyle}>
-                   <span style={this.state.spanHeadStyle}>Study:</span> 
+                   <span style={this.state.spanHeadStyle}>Study:</span>
                    <span style={this.state.spanBodyStyle}> {this.props.studyType}</span>
                 </p>
             </div>
