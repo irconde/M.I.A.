69b065709427828f2f2a8a02fd29b8beaf28d365
irconde <irconde@ualr.edu>
2020-09-11 15:21:24 -0500

    Minor aesthetic changes in top bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -17,14 +17,14 @@ class TopBar extends Component {
                 alignItems: 'center',
                 justifyContent: 'flex-end',
                 color: 'white',
-                boxShadow: '0rem 2rem 8rem 2rem rgba(0, 0, 0, 0.5)'
+                boxShadow: '0.1rem 0.1rem 0.5rem 0.3rem rgba(0, 0, 0, 0.5)'
             },
             imgStyle: {
-                margin: '0.5rem'
+                margin: '1.25rem'
             },
             lastIcon: {
-                margin: '0.5rem 1rem 0.5rem 0'
-            }          
+                margin: '0.5rem 1.5rem 0.5rem 0'
+            }
         }
     }
 
@@ -50,7 +50,7 @@ class TopBar extends Component {
                                 <path d="M27.2380952,6.66666667 L27.2380952,25.3333333 L30.6666667,25.3333333 L30.6666667,6.66666667 L27.2380952,6.66666667 Z M21.9047619,25.3333333 L25.3333333,25.3333333 L25.3333333,6.66666667 L21.9047619,6.66666667 L21.9047619,25.3333333 Z M18.6666667,6.66666667 L2.66666667,6.66666667 C1.93333333,6.66666667 1.33333333,7.26666667 1.33333333,8 L1.33333333,24 C1.33333333,24.7333333 1.93333333,25.3333333 2.66666667,25.3333333 L18.6666667,25.3333333 C19.4,25.3333333 20,24.7333333 20,24 L20,8 C20,7.26666667 19.4,6.66666667 18.6666667,6.66666667 Z" id="Shape" fill="#FFFFFF" fill-rule="nonzero"></path>
                             </g>
                         </g>
-                        <text x="35%" y="50%" fontSize="8.5pt"  fill="black" dominantBaseline="middle" textAnchor="middle">{this.props.numberOfFiles}</text>
+                        <text x="35%" y="53%" fontSize="10pt"  fill="black" dominantBaseline="middle" textAnchor="middle" style={{fontWeight: 600}}>{this.props.numberOfFiles}</text>
                     </svg>
                     <img style={this.state.imgStyle} src='./img/ic_traffic_download_upload.svg' />
                     <img style={this.state.lastIcon} src={this.props.isConnected ? '/img/ic_connection.svg' : '/img/ic_no_connection.svg'} />
@@ -67,7 +67,7 @@ class TopBar extends Component {
                                 <path d="M27.2380952,6.66666667 L27.2380952,25.3333333 L30.6666667,25.3333333 L30.6666667,6.66666667 L27.2380952,6.66666667 Z M21.9047619,25.3333333 L25.3333333,25.3333333 L25.3333333,6.66666667 L21.9047619,6.66666667 L21.9047619,25.3333333 Z M18.6666667,6.66666667 L2.66666667,6.66666667 C1.93333333,6.66666667 1.33333333,7.26666667 1.33333333,8 L1.33333333,24 C1.33333333,24.7333333 1.93333333,25.3333333 2.66666667,25.3333333 L18.6666667,25.3333333 C19.4,25.3333333 20,24.7333333 20,24 L20,8 C20,7.26666667 19.4,6.66666667 18.6666667,6.66666667 Z" id="Shape" fill="#FFFFFF" fill-rule="nonzero"></path>
                             </g>
                         </g>
-                        <text x="35%" y="50%" fontSize="8.5pt"  fill="black" dominantBaseline="middle" textAnchor="middle">{this.props.numberOfFiles}</text>
+                        <text x="35%" y="53%" fontSize="10pt"  fill="black" dominantBaseline="middle" textAnchor="middle" style={{fontWeight: 600}}>{this.props.numberOfFiles}</text>
                     </svg>
                     <img style={this.state.imgStyle} src='./img/ic_traffic_upload.svg' />
                     <img style={this.state.lastIcon} src={this.props.isConnected ? '/img/ic_connection.svg' : '/img/ic_no_connection.svg'} />
@@ -84,7 +84,7 @@ class TopBar extends Component {
                                 <path d="M27.2380952,6.66666667 L27.2380952,25.3333333 L30.6666667,25.3333333 L30.6666667,6.66666667 L27.2380952,6.66666667 Z M21.9047619,25.3333333 L25.3333333,25.3333333 L25.3333333,6.66666667 L21.9047619,6.66666667 L21.9047619,25.3333333 Z M18.6666667,6.66666667 L2.66666667,6.66666667 C1.93333333,6.66666667 1.33333333,7.26666667 1.33333333,8 L1.33333333,24 C1.33333333,24.7333333 1.93333333,25.3333333 2.66666667,25.3333333 L18.6666667,25.3333333 C19.4,25.3333333 20,24.7333333 20,24 L20,8 C20,7.26666667 19.4,6.66666667 18.6666667,6.66666667 Z" id="Shape" fill="#FFFFFF" fill-rule="nonzero"></path>
                             </g>
                         </g>
-                        <text x="35%" y="50%" fontSize="8.5pt"  fill="black" dominantBaseline="middle" textAnchor="middle">{this.props.numberOfFiles}</text>
+                        <text x="35%" y="53%" fontSize="10pt"  fill="black" dominantBaseline="middle" textAnchor="middle" style={{fontWeight: 600}}>{this.props.numberOfFiles}</text>
                     </svg>
                     <img style={this.state.imgStyle} src='./img/ic_traffic_download.svg' />
                     <img style={this.state.lastIcon} src={this.props.isConnected ? '/img/ic_connection.svg' : '/img/ic_no_connection.svg'} />
@@ -101,7 +101,7 @@ class TopBar extends Component {
                                 <path d="M27.2380952,6.66666667 L27.2380952,25.3333333 L30.6666667,25.3333333 L30.6666667,6.66666667 L27.2380952,6.66666667 Z M21.9047619,25.3333333 L25.3333333,25.3333333 L25.3333333,6.66666667 L21.9047619,6.66666667 L21.9047619,25.3333333 Z M18.6666667,6.66666667 L2.66666667,6.66666667 C1.93333333,6.66666667 1.33333333,7.26666667 1.33333333,8 L1.33333333,24 C1.33333333,24.7333333 1.93333333,25.3333333 2.66666667,25.3333333 L18.6666667,25.3333333 C19.4,25.3333333 20,24.7333333 20,24 L20,8 C20,7.26666667 19.4,6.66666667 18.6666667,6.66666667 Z" id="Shape" fill="#FFFFFF" fill-rule="nonzero"></path>
                             </g>
                         </g>
-                        <text x="35%" y="50%" fontSize="8.5pt"  fill="black" dominantBaseline="middle" textAnchor="middle">{this.props.numberOfFiles}</text>
+                        <text x="35%" y="53%" fontSize="10pt"  fill="black" dominantBaseline="middle" textAnchor="middle" style={{fontWeight: 600}}>{this.props.numberOfFiles}</text>
                     </svg>
                     <img style={this.state.imgStyle} src='./img/ic_traffic_no_transmission.svg' />
                     <img style={this.state.lastIcon} src={this.props.isConnected ? '/img/ic_connection.svg' : '/img/ic_no_connection.svg'} />
