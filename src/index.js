ae219c2b1b248f1632af5038f198b0e1e297c499
Dylan Johnson <marcusdylanjohnson@gmail.com>
2021-02-10 14:41:47 -0600

    (temp) Use isolated component for redux tests

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReduxTest from './components/reduxTest';
+import { Provider } from 'react-redux';
+import store from './redux/store/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <ReduxTest />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
