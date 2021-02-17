cef37122d102b3cc0ddab4b4c9b674b74b684a89
irconde <irconde@ualr.edu>
2021-02-17 17:05:44 -0600

    Identation fixed

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -16,7 +16,7 @@ const TopBar = ({ numberOfFiles, isUpload, isDownload, isConnected , connectedSe
             width: '100%',
             zIndex: '999',
             alignItems: 'center',
-           justifyContent: 'flex-end',
+            justifyContent: 'flex-end',
             color: 'white',
             boxShadow: '0.1rem 0.1rem 0.5rem 0.3rem rgba(0, 0, 0, 0.5)',
         },
@@ -31,9 +31,9 @@ const TopBar = ({ numberOfFiles, isUpload, isDownload, isConnected , connectedSe
             zIndex: '999',
             alignItems: 'center',
             color: 'white',
-			justifyContent: 'center',
-           	
-		},
+            justifyContent: 'center',
+
+        },
         connectionStatusIconsContainer:{
             position: 'absolute',
             display: 'flex',
@@ -42,52 +42,49 @@ const TopBar = ({ numberOfFiles, isUpload, isDownload, isConnected , connectedSe
             top: '0',
             width: '35%',
             zIndex: '999',
-			'margin-left':'65%',
+            marginLeft:'65%',
             alignItems: 'center',
             justifyContent: 'flex-end',
             color: 'white',
-           	
-		},
+
+        },
         icon: {
             margin: '1.25rem',
-			
         },
         lastIcon: {
             margin: '0.5rem 2.5rem 0.5rem 0rem',
-			
         },
-		connectionFile: {
-			color: '#C3C3C3',
+        connectionFile: {
+            color: '#C3C3C3',
         }
     };
 
     return (
-		<div >
-        <div style={styles.titleLabelContainer}>
-			<p style={styles.connectionFile}>Connected to </p>&nbsp;&nbsp;
-			<h4 >{connectedServer}</h4> &nbsp;&nbsp;
-			<p>/</p>&nbsp;&nbsp;
-			<p style={styles.connectionFile}>Processing</p>&nbsp;&nbsp;
-			<h4 >{processingFile}</h4>
+        <div >
+            <div style={styles.titleLabelContainer}>
+                <p style={styles.connectionFile}>Connected to </p>&nbsp;&nbsp;
+                <h4 >{connectedServer}</h4> &nbsp;&nbsp;
+                <p>/</p>&nbsp;&nbsp;
+                <p style={styles.connectionFile}>Processing</p>&nbsp;&nbsp;
+                <h4 >{processingFile}</h4>
+            </div>
+            <div style={styles.connectionStatusIconsContainer}>
+                <FileQueueIcon
+                    title="Number of Files"
+                    numberOfFiles={numberOfFiles}
+                    style={styles.icon}
+                />
+                <FileUploadStatus
+                    isDownload={isDownload}
+                    isUpload={isUpload}
+                    styles={styles.icon}
+                />
+                <ConnectionStatus
+                    isConnected={isConnected}
+                    style={styles.lastIcon}
+                />
+            </div>
         </div>
-		
-	    <div style={styles.connectionStatusIconsContainer}>
-				<FileQueueIcon
-					title="Number of Files"
-					numberOfFiles={numberOfFiles}
-					style={styles.icon}
-				/>
-				<FileUploadStatus
-					isDownload={isDownload}
-					isUpload={isUpload}
-					styles={styles.icon}
-				/>
-				<ConnectionStatus
-					isConnected={isConnected}
-					style={styles.lastIcon}
-				/>
-        </div>
-		</div>
     );
 };
 
@@ -96,8 +93,8 @@ TopBar.propTypes = {
     isUpload: PropTypes.bool,
     isDownload: PropTypes.bool,
     isConnected: PropTypes.bool,
-	connectedServer: PropTypes.string,
-	processingFile: PropTypes.string
+    connectedServer: PropTypes.string,
+    processingFile: PropTypes.string
 };
 
 export default TopBar;
