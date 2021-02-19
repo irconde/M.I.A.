9250f41c7aaab1c59c1b27ba38ecf9d9221d01a9
James <jxbromley@ualr.edu>
2021-02-19 10:30:33 -0600

    Commenting and correcting range issue when detecting screen size in the Bounding Polygon FAB.

--- src/components/FAB/BoundPolyFAB.js
+++ src/components/FAB/BoundPolyFAB.js
@@ -89,6 +89,7 @@ const BoundPolyFAB = ({
     } else if (Utils.inRange(width, 2001, 3000)) {
         leftPX = '51.5%';
     }
+
     let fabOpacity;
     if (cornerstoneMode === constants.cornerstoneMode.ANNOTATION) {
         fabOpacity = false;
