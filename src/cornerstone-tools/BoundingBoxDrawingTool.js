fb3663715e94b8a221719751e79447a0f3c6f958
James <jxbromley@ualr.edu>
2021-02-19 10:55:44 -0600

    Formatting

--- src/cornerstone-tools/BoundingBoxDrawingTool.js
+++ src/cornerstone-tools/BoundingBoxDrawingTool.js
@@ -120,7 +120,6 @@ export default class BoundingBoxDrawingTool extends BaseAnnotationTool {
                     drawHandlesIfActive: drawHandlesOnHover,
                     hideHandlesIfMoving,
                 };
-
                 setShadow(context, this.configuration);
                 const rectOptions = { color };
 
