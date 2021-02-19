a945459435ba9d8d0491b22e19f010486923c8fa
James <jxbromley@ualr.edu>
2021-02-19 16:08:19 -0600

    Modified Cornerstone Tool BoundingBoxDrawing bounding box line width to the constant used for other detections line width

--- src/cornerstone-tools/BoundingBoxDrawingTool.js
+++ src/cornerstone-tools/BoundingBoxDrawingTool.js
@@ -116,6 +116,7 @@ export default class BoundingBoxDrawingTool extends BaseAnnotationTool {
                     drawHandlesIfActive: drawHandlesOnHover,
                     hideHandlesIfMoving,
                 };
+
                 setShadow(context, this.configuration);
                 const rectOptions = { color };
 
