060e70b1a5d9ca95768041c5e5b55390f96fab7f
James <jxbromley@ualr.edu>
2022-04-13 16:08:01 -0500

    Correcting missing px values

--- src/components/LazyImage/LazyImageContainer.js
+++ src/components/LazyImage/LazyImageContainer.js
@@ -18,7 +18,7 @@ const ImageContainer = styled.div`
     border: ${(props) =>
         props.selected ? '4px solid #367eff' : '1px solid fff'};
     overflow-x: hidden;
-    margin: 0 16 60 0;
+    margin: 0 16px 60px 0;
     border-radius: 6px;
     background-color: #242424;
     justify-content: center;
