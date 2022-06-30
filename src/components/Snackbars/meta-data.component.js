b8cbac686ec23c82d7ca99be4e1d91130fa837fe
Dako Albeik <mfalbeik@ualr.edu>
2022-06-30 16:03:38 -0500

    Refactor styles for meta-data.component

--- src/components/Snackbars/meta-data.component.js
+++ src/components/Snackbars/meta-data.component.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Info } from '../side-menu/Icons';
 import { useSelector } from 'react-redux';
 import {
     getCollapsedSideMenu,
     getConfigInfo,
 } from '../../redux/slices/ui/uiSlice';
 import { getSelectedAlgorithm } from '../../redux/slices/detections/detectionsSlice';
+import {
+    InlineSlash,
+    MetaDataText,
+    MetaDataGroup,
+    StyledMetaData,
+    StyledInfoIcon,
+} from './meta-data.styles';
 
 /**
  * @component
@@ -20,71 +26,36 @@ const MetaData = () => {
     const isVisible = selectedAlgorithm !== '' ? true : false;
     const configInfo = useSelector(getConfigInfo);
     const sideMenuCollapsed = useSelector(getCollapsedSideMenu);
-    const slashLineStyle = {
-        fontFamily: 'Noto Sans JP Black',
-        color: '#367FFF',
-        display: 'inline-block',
-    };
-    const spanHeadStyle = {
-        fontFamily: 'Noto Sans JP ',
-    };
-    const spanBodyStyle = {
-        fontFamily: 'Noto Sans JP ',
-    };
-    const paragraphStyle = {
-        margin: '0.6rem 0.6rem 0.6rem 0.6rem',
-        display: 'inline-block',
-        fontSize: '14',
-        color: 'white',
-    };
-    const divStyle = {
-        paddingLeft: '1rem',
-        paddingRight: '1rem',
-        position: 'fixed',
-        top: '5rem',
-        // Width of screen - side menu width / 2
-        left: '50%',
-        transform:
-            sideMenuCollapsed === true ? 'translate(-50%)' : 'translate(-68%)',
-        backgroundColor: 'rgba(38, 38, 38, 0.5)',
-        borderRadius: '2.0rem',
-        textAlign: 'left',
-        color: '#ffffff',
-        width: 'max-content',
-    };
 
     if (!isVisible) {
-        return <div></div>;
+        return <></>;
     } else {
         return (
-            <div style={divStyle}>
-                <Info style={{ verticalAlign: 'text-top' }} />
-                <p style={paragraphStyle}>
-                    <span style={spanHeadStyle}>Detector Type:</span>
-                    <span style={spanBodyStyle}>
-                        {' '}
-                        {configInfo.detectorType}
-                    </span>
-                </p>
-                <p style={slashLineStyle}>/</p>
-                <p style={paragraphStyle}>
-                    <span style={spanHeadStyle}>Detector Configuration:</span>
-                    <span style={spanBodyStyle}>
+            <StyledMetaData sideMenuCollapsed={sideMenuCollapsed}>
+                <StyledInfoIcon />
+                <MetaDataGroup>
+                    <MetaDataText>Detector Type:</MetaDataText>
+                    <MetaDataText> {configInfo.detectorType}</MetaDataText>
+                </MetaDataGroup>
+                <InlineSlash>/</InlineSlash>
+                <MetaDataGroup>
+                    <MetaDataText>Detector Configuration:</MetaDataText>
+                    <MetaDataText>
                         {' '}
                         {configInfo.detectorConfigType}
-                    </span>
-                </p>
-                <p style={slashLineStyle}>/</p>
-                <p style={paragraphStyle}>
-                    <span style={spanHeadStyle}>Series:</span>
-                    <span style={spanBodyStyle}> {configInfo.seriesType}</span>
-                </p>
-                <p style={slashLineStyle}>/</p>
-                <p style={paragraphStyle}>
-                    <span style={spanHeadStyle}>Study:</span>
-                    <span style={spanBodyStyle}> {configInfo.studyType}</span>
-                </p>
-            </div>
+                    </MetaDataText>
+                </MetaDataGroup>
+                <InlineSlash>/</InlineSlash>
+                <MetaDataGroup>
+                    <MetaDataText>Series:</MetaDataText>
+                    <MetaDataText> {configInfo.seriesType}</MetaDataText>
+                </MetaDataGroup>
+                <InlineSlash>/</InlineSlash>
+                <MetaDataGroup>
+                    <MetaDataText>Study:</MetaDataText>
+                    <MetaDataText> {configInfo.studyType}</MetaDataText>
+                </MetaDataGroup>
+            </StyledMetaData>
         );
     }
 };
---
+++ src/components/Snackbars/meta-data.styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { Info } from '../side-menu/Icons';
+
+export const InlineSlash = styled.p`
+    font-family: Noto Sans JP Black;
+    color: #367fff;
+    display: inline-block;
+`;
+export const MetaDataText = styled.span`
+    font-family: Noto Sans JP;
+`;
+export const MetaDataGroup = styled.p`
+    margin: 0.6rem 0.6rem 0.6rem 0.6rem;
+    display: inline-block;
+    font-size: 14;
+    color: white;
+`;
+export const StyledMetaData = styled.div`
+    padding-left: 1rem;
+    padding-right: 1rem;
+    position: fixed;
+    top: 5rem;
+    // Width of screen - side menu width / 2
+    left: 50%;
+    transform: ${(props) =>
+        props.sideMenuCollapsed === true
+            ? 'translate(-50%)'
+            : 'translate(-68%)'};
+    background-color: rgba(38, 38, 38, 0.5);
+    border-radius: 2rem;
+    text-align: left;
+    color: #ffffff;
+    width: max-content;
+`;
+export const StyledInfoIcon = styled(Info)`
+    vertical-align: text-top;
+`;
