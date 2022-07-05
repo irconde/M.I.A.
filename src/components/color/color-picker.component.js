588b88f4aedc0c2e32d2890b78e5422e89c5120d
James <jxbromley@ualr.edu>
2022-07-05 16:41:32 -0500

    Small refactor of style calculating in color-picker.component.js

---
+++ src/components/color/color-picker.component.js
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from 'react';
+import { TwitterPicker } from 'react-color';
+import { useDispatch, useSelector } from 'react-redux';
+import * as constants from '../../utils/Constants';
+import { detectionContextStyle } from '../../utils/Constants';
+import {
+    addMissMatchedClassName,
+    getSelectedDetectionClassName,
+    getSelectedDetectionViewport,
+    getSelectedDetectionWidthAndHeight,
+} from '../../redux/slices/detections/detectionsSlice';
+import {
+    getColorPickerVisible,
+    getDetectionContextPosition,
+    getZoomLevels,
+} from '../../redux/slices/ui/uiSlice';
+
+/**
+ * Component within DetectionContextMenu component for editing colors of detection bounding box.
+ *
+ * @component
+ */
+
+const ColorPickerComponent = () => {
+    const isVisible = useSelector(getColorPickerVisible);
+    const detectionContextPosition = useSelector(getDetectionContextPosition);
+    const zoomLevels = useSelector(getZoomLevels);
+    const selectedViewport = useSelector(getSelectedDetectionViewport);
+    const widthAndHeight = useSelector(getSelectedDetectionWidthAndHeight);
+    const [topPosition, setTopPosition] = useState();
+    const [leftPosition, setLeftPosition] = useState();
+    useEffect(() => {
+        if (widthAndHeight !== null) {
+            let menuOffset = 37;
+            if (selectedViewport === constants.viewport.TOP) {
+                setTopPosition(
+                    detectionContextPosition.top +
+                        zoomLevels.zoomLevelTop +
+                        detectionContextStyle.HEIGHT +
+                        10
+                );
+                setLeftPosition(
+                    detectionContextPosition.left +
+                        zoomLevels.zoomLevelTop +
+                        menuOffset
+                );
+            } else if (selectedViewport === constants.viewport.SIDE) {
+                setTopPosition(
+                    detectionContextPosition.top +
+                        zoomLevels.zoomLevelSide +
+                        detectionContextStyle.HEIGHT +
+                        10
+                );
+                setLeftPosition(
+                    detectionContextPosition.left +
+                        zoomLevels.zoomLevelSide +
+                        menuOffset
+                );
+            }
+        }
+    }, [widthAndHeight]);
+    const selectedDetectionClassName = useSelector(
+        getSelectedDetectionClassName
+    );
+    const dispatch = useDispatch();
+    const [color, setColor] = useState();
+
+    const colorChangeComplete = (color) => {
+        setColor(color);
+        dispatch(
+            addMissMatchedClassName({
+                className: selectedDetectionClassName,
+                color: color.hex,
+            })
+        );
+    };
+    if (isVisible === true) {
+        return (
+            <div
+                style={{
+                    position: 'absolute',
+                    top: topPosition,
+                    left: leftPosition,
+                }}>
+                <TwitterPicker
+                    color={color}
+                    onChangeComplete={(color) => colorChangeComplete(color)}
+                    triangle="top-left"
+                />
+            </div>
+        );
+    } else {
+        return null;
+    }
+};
+
+export default ColorPickerComponent;
