38286a3753de1b6c2ae0039b656881769250d1f8
Dako Albeik <mfalbeik@ualr.edu>
2022-08-02 16:13:47 -0500

    remove specific styles from info icon

--- src/icons/meta-data/info-icon/info.icon.styles.js
+++ src/icons/meta-data/info-icon/info.icon.styles.js
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 import { iconColor, iconSize } from '../../shared/24px.icon.styles';
 
 export const StyledInfoIcon = styled(InfoIconComponent).attrs(iconSize)`
-    align-self: center;
-    cursor: pointer;
     vertical-align: text-top;
     ${iconColor}
 `;
