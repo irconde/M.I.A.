49be882d332c1a3c5e8125808b2a98047f43fd52
Dako Albeik <mfalbeik@ualr.edu>
2022-08-02 14:45:48 -0500

    Edit label, and lazy image menu

--- src/icons/lazy-image-menu/lazy-menu-toggle-icon/lazy-menu-toggle.icon.styles.js
+++ src/icons/lazy-image-menu/lazy-menu-toggle-icon/lazy-menu-toggle.icon.styles.js
@@ -1,16 +1,14 @@
 import styled from 'styled-components';
 import { ReactComponent as LazyMenuToggleIconComponent } from './lazy-menu-toggle.icon.svg';
+import { iconColor, iconSize } from '../../shared/24px.icon.styles';
 
 export const StyledLazyMenuToggleIcon = styled(
     LazyMenuToggleIconComponent
-).attrs((props) => ({
-    width: props.width || '32px',
-    height: props.height || '32px',
-}))`
+).attrs(iconSize)`
     align-self: center;
-    fill: ${(props) => props.color};
     margin-top: auto;
     margin-bottom: auto;
     transform: rotate(180);
     cursor: pointer;
+    ${iconColor}
 `;
