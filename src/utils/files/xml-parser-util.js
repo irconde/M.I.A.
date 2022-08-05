b3c4ab2e0a21567915401b07af0a0dbb29195906
James <jxbromley@ualr.edu>
2022-08-05 08:55:24 -0500

    Updating xml-parser-util class. Added return definition for parsing xml file. Removed unneeded format specific functions.

--- src/utils/files/xml-parser-util.js
+++ src/utils/files/xml-parser-util.js
@@ -1,5 +1,3 @@
-import { SETTINGS } from '../enums/Constants';
-
 /**
  * Class that parses a xml file based on the given string
  */
@@ -17,7 +15,7 @@ export default class XmlParserUtil {
 
     /**
      * Returns the xml string in a simply javascript object
-     * @returns {Object}
+     * @returns {{format: string; views: Array<{view: string; pixelData: string; detectionData: Array<string>}>;}}
      */
     getParsedXmlData() {
         // TODO: Simply return an easily readable and parsable json object of the xml data
@@ -25,28 +23,5 @@ export default class XmlParserUtil {
         this._xmlDoc = parser.parseFromString(this._xmlData, 'text/xml');
         const xmlImage = this._xmlDoc.getElementsByTagName('image');
         const currentFileFormat = xmlImage[0].getAttribute('format');
-        if (currentFileFormat === SETTINGS.ANNOTATIONS.COCO) {
-            return this._parseCocoData();
-        } else if (currentFileFormat === SETTINGS.ANNOTATIONS.TDR) {
-            return this._parseTdrData();
-        }
-    }
-
-    /**
-     * Returns the parsed data of a coco formatted file
-     * @returns {string}
-     * @private
-     */
-    _parseCocoData() {
-        return 'TODO: Return parsed coco data';
-    }
-
-    /**
-     * Returns the parsed data of a tdr/dicos formatted file
-     * @returns {string}
-     * @private
-     */
-    _parseTdrData() {
-        return 'TODO: Return parsed tdr data';
     }
 }
