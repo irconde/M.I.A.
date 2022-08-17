a3ed68b905934992b0a27ab0e4750aebe38f6089
Dako Albeik <mfalbeik@ualr.edu>
2022-08-17 09:47:46 -0500

    Create helper function to parse COCO data

--- src/utils/files/file-utils.js
+++ src/utils/files/file-utils.js
@@ -1,6 +1,7 @@
 import JSZip from 'jszip';
 import XmlParserUtil from './xml-parser-util';
 import { SETTINGS } from '../enums/Constants';
+import { v4 as uuidv4 } from 'uuid';
 
 /**
  * Class that manages the loading and generating of files for the application
@@ -35,41 +36,43 @@ export default class FileUtils {
                     const parsedData = this._xmlParser.getParsedXmlData();
                     console.log(parsedData);
                     if (parsedData.format === SETTINGS.ANNOTATIONS.COCO) {
-                        const detectionData = [];
-                        parsedData.views.forEach((view) => {
-                            view.detectionData.forEach((detectionPath) => {
-                                zipUtil
-                                    .file(detectionPath)
-                                    .async('string')
-                                    .then((string) => {
-                                        const detection = JSON.parse(string);
-                                        console.log(detection);
-                                        const { annotations, info } = detection;
-                                        const {
-                                            className,
-                                            confidence,
-                                            bbox,
-                                            id,
-                                            image_id,
-                                        } = annotations[0];
-                                        detectionData.push({
-                                            algorithm: info.algorithm,
-                                            className,
-                                            confidence,
-                                            view: view.view,
-                                            boundingBox: bbox,
-                                            binaryMask: [],
-                                            polygonMask: [],
-                                            uuid: id, // make sure this is the right id
-                                            detectionFromFile: true,
-                                            imageId: image_id,
-                                        });
-                                        console.log(detectionData);
-                                    });
-                            });
-                        });
+                        this._loadCOCOdata(parsedData, zipUtil).then((data) =>
+                            console.log(data)
+                        );
                     }
                 });
         });
+    }
+
+    async _loadCOCOdata(parsedData, zipUtil) {
+        const detectionData = [];
+        parsedData.views.forEach((view) => {
+            view.detectionData.forEach((detectionPath) => {
+                zipUtil
+                    .file(detectionPath)
+                    .async('string')
+                    .then((string) => {
+                        const detection = JSON.parse(string);
+                        console.log(detection);
+                        const { annotations, info } = detection;
+                        const { className, confidence, bbox, id, image_id } =
+                            annotations[0];
+                        detectionData.push({
+                            algorithm: info.algorithm,
+                            className,
+                            confidence,
+                            view: view.view,
+                            boundingBox: bbox,
+                            // To be calculated
+                            binaryMask: [],
+                            polygonMask: [],
+                            uuid: uuidv4(), // make sure this is the right id
+                            detectionFromFile: true,
+                            imageId: image_id,
+                        });
+                    });
+            });
+        });
+        return detectionData;
     }
 }
