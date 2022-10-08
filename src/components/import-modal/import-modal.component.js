7fa3c3dbfe6b375d6c050200dab3cc29ededd96a
Dako Albeik <mfalbeik@ualr.edu>
2022-10-07 20:08:11 -0500

    Create getNextFile function

--- src/components/import-modal/import-modal.component.js
+++ src/components/import-modal/import-modal.component.js
@@ -122,19 +122,19 @@ const ImportModalComponent = (props) => {
                     selectedImagesDirPath: paths.images,
                     selectedAnnotationsDirPath: paths.annotations,
                 })
-            ).then(async () => {
-                try {
-                    const fileData = await ipcRenderer.invoke(
-                        Channels.getNextFile
-                    );
-                    console.log(fileData);
-                } catch (e) {
-                    // TODO: no file
-                    console.log(e);
-                }
-            });
+            ).then(getNextFile);
 
             handleClose();
+        }
+    };
+
+    const getNextFile = async () => {
+        try {
+            const fileData = await ipcRenderer.invoke(Channels.getNextFile);
+            console.log(fileData);
+        } catch (e) {
+            // TODO: no file
+            console.log(e);
         }
     };
 
