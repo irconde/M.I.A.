2b5e4fcb48b1857c11f8c06c22c1eb0258a6ae8d
Dako Albeik <mfalbeik@ualr.edu>
2022-10-10 18:03:45 -0500

    Render image display component

--- src/components/image-display/image-display.component.js
+++ src/components/image-display/image-display.component.js
@@ -2,9 +2,11 @@ import * as cornerstoneTools from 'eac-cornerstone-tools';
 import * as cornerstone from 'cornerstone-core';
 import Hammer from 'hammerjs';
 import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
+import * as cornerstoneWebImageLoader from 'cornerstone-web-image-loader';
 import dicomParser from 'dicom-parser';
 import Utils from '../../utils/general/Utils';
 import React, { createRef } from 'react';
+import * as cornerstoneMath from 'cornerstone-math';
 
 cornerstoneTools.external.cornerstone = cornerstone;
 cornerstoneTools.external.Hammer = Hammer;
