329c5e32b6665ca7cb37e85995018928844b7c01
James <jxbromley@ualr.edu>
2022-10-10 12:22:18 -0500

    Bootstrapping cornerstone tools etc

---
+++ src/components/image-display/image-display.component.js
@@ -0,0 +1,71 @@
+import * as cornerstoneTools from 'eac-cornerstone-tools';
+import * as cornerstone from 'cornerstone-core';
+import Hammer from 'hammerjs';
+import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
+import dicomParser from 'dicom-parser';
+import Utils from '../../utils/general/Utils';
+import React, { createRef } from 'react';
+
+cornerstoneTools.external.cornerstone = cornerstone;
+cornerstoneTools.external.Hammer = Hammer;
+cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
+cornerstoneTools.init({
+    mouseEnabled: true,
+    touchEnabled: true,
+});
+cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
+cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
+cornerstoneWADOImageLoader.webWorkerManager.initialize({
+    maxWebWorkers: navigator.hardwareConcurrency || 1,
+    startWebWorkersOnDemand: true,
+    taskConfiguration: {
+        decodeTask: {
+            initializeCodecsOnStartup: false,
+            usePDFJS: false,
+            strict: false,
+        },
+    },
+});
+cornerstoneWebImageLoader.external.cornerstone = cornerstone;
+cornerstone.registerImageLoader('myCustomLoader', Utils.loadImage);
+
+const ImageDisplayComponent = () => {
+    const viewportRef = createRef();
+    const setupCornerstoneJS = () => {
+        cornerstone.enable(viewportRef);
+        const PanTool = cornerstoneTools.PanTool;
+        cornerstoneTools.addTool(PanTool);
+        cornerstoneTools.setToolActive('Pan', { mouseButtonMask: 1 });
+        const Zoom = cornerstoneTools.ZoomMouseWheelTool;
+        cornerstoneTools.addTool(Zoom);
+        cornerstoneTools.setToolActive('ZoomMouseWheel', {});
+        const ZoomTouchPinchTool = cornerstoneTools.ZoomTouchPinchTool;
+        cornerstoneTools.addTool(ZoomTouchPinchTool);
+        cornerstoneTools.setToolActive('ZoomTouchPinch', {});
+    };
+    setupCornerstoneJS();
+
+    return (
+        <div className="imageViewport" id="imageContainer" ref={viewportRef}>
+            <div
+                id="viewerContainer"
+                style={{
+                    width: '100vw',
+                    height: '100vh',
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                }}
+                onContextMenu={(e) => e.preventDefault()}
+                className="disable-selection noIbar"
+                unselectable="off"
+                ref={(el) => {
+                    el &&
+                        el.addEventListener('selectstart', (e) => {
+                            e.preventDefault();
+                        });
+                }}></div>
+        </div>
+    );
+};
+
+export default ImageDisplayComponent;
