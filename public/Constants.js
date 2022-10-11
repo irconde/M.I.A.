84027afb5454c4288b5b5e48650f97b643724e8d
James <jxbromley@ualr.edu>
2022-10-11 11:59:57 -0500

    Small changes

--- public/Constants.js
+++ public/Constants.js
@@ -43,14 +43,6 @@ module.exports.Thumbnail = {
 };
 
 module.exports.defaultSettings = {
-    remoteIp: '127.0.0.1',
-    remotePort: '4001',
-    autoConnect: true,
-    fileFormat: 'Open Raster',
-    annotationsFormat: 'DICOS TDR',
-    localFileOutput: '',
-    fileSuffix: '_img',
-    deviceType: '',
     selectedImagesDirPath: '',
     selectedAnnotationsDirPath: '',
 };
--- src/components/image-display/image-display.styles.js
+++ src/components/image-display/image-display.styles.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 export const ImageViewport = styled.div.attrs(() => ({
     id: 'imageContainer',
 }))`
-    width: 100vw;
     height: 100vh;
+    width: 100vw;
+    top: 0;
+    left: 0;
+    position: absolute;
 `;
