7e41574bae4f0950267c2ef9e5634359ea71324f
James <jxbromley@ualr.edu>
2023-01-18 18:00:55 -0600

    Fixing loading errors

--- src/components/image-display/image-display.component.js
+++ src/components/image-display/image-display.component.js
@@ -82,16 +82,19 @@ const ImageDisplayComponent = () => {
                 onImageRenderedHandler
             );
         };
-    }, [selectedImagesDirPath, pixelData, annotations]);
+    }, [selectedImagesDirPath, pixelData]);
 
     useEffect(() => {
         getNextFile()
             .then((data) => {
+                console.log(data);
                 const { pixelData, annotationInformation } = data;
                 dispatch(addAnnotationArray(annotationInformation));
                 setPixelData(pixelData);
             })
-            .catch((error) => {});
+            .catch((error) => {
+                console.log(error);
+            });
     }, []);
 
     const getNextFile = async () => {
@@ -171,16 +174,24 @@ const ImageDisplayComponent = () => {
     const displayImage = async (pixelData) => {
         try {
             const imageIdTop = 'coco:0';
-            Utils.loadImage(imageIdTop, pixelData).then((image) => {
-                const viewport = cornerstone.getDefaultViewportForImage(
-                    viewportRef.current,
-                    image
-                );
-                viewport.translation.y = constants.viewportStyle.ORIGIN;
-                viewport.scale = 1.2;
-                setViewport(viewport);
-                cornerstone.displayImage(viewportRef.current, image, viewport);
-            });
+            Utils.loadImage(imageIdTop, pixelData)
+                .then((image) => {
+                    const viewport = cornerstone.getDefaultViewportForImage(
+                        viewportRef.current,
+                        image
+                    );
+                    viewport.translation.y = constants.viewportStyle.ORIGIN;
+                    viewport.scale = 1.2;
+                    setViewport(viewport);
+                    cornerstone.displayImage(
+                        viewportRef.current,
+                        image,
+                        viewport
+                    );
+                })
+                .catch((err) => {
+                    setError(err.message);
+                });
             // clear error if there is one
             error && setError('');
         } catch (e) {
