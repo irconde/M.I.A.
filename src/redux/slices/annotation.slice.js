1b72cededd31126087e1ffb5f6d377880e4c7682
James Bromley <jxbromley@ualr.edu>
2023-03-23 14:39:23 -0500

    Correctly de-selecting non-selected category annotations

--- src/redux/slices/annotation.slice.js
+++ src/redux/slices/annotation.slice.js
@@ -430,7 +430,8 @@ const annotationSlice = createSlice({
                     if (annotation.categorySelected && annotation.selected) {
                         annotation.selected = false;
                     }
-                } else if (annotation.categorySelected === true) {
+                } else {
+                    annotation.selected = false;
                     annotation.categorySelected = false;
                 }
             });
